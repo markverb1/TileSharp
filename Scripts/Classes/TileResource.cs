9964563faba307b11fd6702f6b6911b8e778b4ff
markverb1 <77983517+markverb1@users.noreply.github.com>
2026-04-18 10:41:51 +0300

    Add tileresource class

--- Scripts/Classes/TileResource.cs
+++ Scripts/Classes/TileResource.cs
@@ -1,7 +1,11 @@
 using Godot;
 using System;
+using TileSharp.Components;
 
+[GlobalClass]
 public partial class TileResource : Resource
 {
-    
+    [Export] public Texture TileTexture;
+
+    [Export] public Godot.Collections.Array<TileGridComponent.Mask> Mapping;
 }
